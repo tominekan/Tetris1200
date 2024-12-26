cdbc0119086f628f735452a348cafddff5a279a3
Tomi Adenekan <tominekan12@gmail.com>
2024-12-26 04:39:53 -0800

    updated build.zsh to add the actual main jar file lol

--- build.zsh
+++ build.zsh
@@ -1,1 +1,1 @@
-jpackage --icon assets/Tetris1200.icns --name Tetris1200 --type dmg --app-version "1.0" --input .
+jpackage --icon assets/Tetris1200.icns --name Tetris1200 --type dmg --app-version "1.0" --input . --main-jar Tetris1200.jar
